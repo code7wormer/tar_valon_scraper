9f4ca25a617bc5ea02c48bc39bc65af3c0a4bf47
code7wormer <shardulsingh.01@gmail.com>
2026-07-27 03:05:48 +0530

    add books

---
+++ chapter_scraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+# url="https://library.tarvalon.net/index.php?title=The_Great_Hunt:_Chapter_3"
+def scrape(url):
+  response=requests.get(url)
+  txt=""
+  soup=BeautifulSoup(response.text,"html.parser")
+  content=soup.find(id="mw-content-text")
+  #mw-content-text
+  heading = soup.select_one("#firstHeading").get_text(" ", strip=True) +"\n\n"
+  txt+=f"#{heading.upper()}
+  first_p = content.find("p")
+  if first_p and "Next Chapter" in first_p.get_text():
+    first_p.decompose()
+  collect=True
+  for child in content.find_all(recursive=False):
+      if child.get("id")=="toc":
+        collect=False
+      if child.name=="h2":
+        section=child.find("span").get("id")
+        if section=="Outline":
+          collect=True
+        if section=="Spoilers":
+          collect=False
+          break
+      if collect:
+        # txt=txt+child.get_text(" ", strip=True)+"\n\n"
+            if child.name == "h2":
+                heading = child.get_text(" ", strip=True)
+                txt += f"#{heading}\n\n"
+
+            elif child.name == "h3":
+                heading = child.get_text(" ", strip=True)
+                txt += f"##{heading}\n\n"
+
+            elif child.name == "p":
+                txt += child.get_text(" ", strip=True) + "\n\n"
+
+            elif child.name in ["ul", "ol"]:
+                for li in child.find_all("li", recursive=False):
+                    txt += "- " + li.get_text(" ", strip=True) + "\n"
+                txt += "\n"
+
+
+  print(txt)
+      
--- main.py
+++ main.py
@@ -1,45 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://library.tarvalon.net/index.php?title=The_Great_Hunt:_Chapter_3"
+from urllib.parse import urljoin
+url="https://library.tarvalon.net/index.php?title=Chapter_Summaries"
 response=requests.get(url)
-txt=""
 soup=BeautifulSoup(response.text,"html.parser")
 content=soup.find(id="mw-content-text")
-#mw-content-text
-heading = soup.select_one("#firstHeading").get_text(" ", strip=True) +"\n\n"
-txt+=f"#{heading.upper()}
-first_p = content.find("p")
-if first_p and "Next Chapter" in first_p.get_text():
-  first_p.decompose()
-collect=True
-for child in content.find_all(recursive=False):
-    if child.get("id")=="toc":
-      collect=False
-    if child.name=="h2":
-      section=child.find("span").get("id")
-      if section=="Outline":
-        collect=True
-      if section=="Spoilers":
-        collect=False
-        break
-    if collect:
-      # txt=txt+child.get_text(" ", strip=True)+"\n\n"
-          if child.name == "h2":
-              heading = child.get_text(" ", strip=True)
-              txt += f"#{heading}\n\n"
-
-          elif child.name == "h3":
-              heading = child.get_text(" ", strip=True)
-              txt += f"##{heading}\n\n"
-
-          elif child.name == "p":
-              txt += child.get_text(" ", strip=True) + "\n\n"
-
-          elif child.name in ["ul", "ol"]:
-              for li in child.find_all("li", recursive=False):
-                  txt += "- " + li.get_text(" ", strip=True) + "\n"
-              txt += "\n"
+books={}
+for child in content.find_all("li"):
+  link=child.find("a")
+  book_name=link.get_text(" ",strip=True)
+  book_rel_link=link.get("href")
+  book_url=urljoin(url,book_rel_link)
+  books[book_name]=book_url
+url2="https://library.tarvalon.net/index.php?title=The_Eye_of_the_World:_Chapter_Summaries"#selected book
 
 
-print(txt)
-    
+
+
